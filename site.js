da12d18308e88901f0e3ed5aeced710e9abb75e0
Sara Lopez <slopez12@hawk.iit.edu>
2017-09-14 22:15:53 -0500

    get location input

--- site.js
+++ site.js
@@ -7,16 +7,19 @@ var buildings = [{
   "name": "S.R.Crown Hall",
   "address": "3360 S State St, Chicago, IL",
   "location":"https://www.google.com/maps/place/S.+R.+Crown+Hall/@41.8343335,-87.6275423,17z/data=!3m1!5s0x880e2c0d0aad0f2f:0x18bd61b0ced378cc!4m12!1m6!3m5!1s0x0:0x91cbe4661afb6f1a!2sIllinois+Institute+of+Technology!8m2!3d41.8348731!4d-87.6270059!3m4!1s0x0:0x70210951aac6ba0f!8m2!3d41.8331937!4d-87.627275"
-}
+},
+get location() {
+			return this.name.address.location
+			}
 ]
 
 /* $('#building-form').append('<img src="' + building['avatar_url'] +'" />');
 building image?*/
 
 $('#building-form').on('submit', function(e) {
-  var building-name = $('#building-name').val();
-  $('#results').append('<li><a href="https://www.google.com/maps/place/' + building-name + '">' +
-  building-name + '</a></li>');
+  var buildingname = $('#buildingname').val();
+  $('#results').append('<li><a href="https://www.google.com/maps/place/' +
+  buildingname + '">' + '</a></li>');
   e.preventDefault();
 });
 $('#building-name').on('focus', function() {
